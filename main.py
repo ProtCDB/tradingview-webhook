65595ccaad86186242dae07529ab7222896577c1
ProtCDB <luismage81@gmail.com>
2025-05-31 13:45:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.json
-    print("✅ Webhook recibido:", data)
+    print("🔔 Alerta recibida de TradingView:")
+    print(data)
     return 'OK', 200
 
-@app.route('/')
-def index():
-    return 'Servidor activo', 200
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))  # Render asigna el puerto en esta variable
+    app.run(host='0.0.0.0', port=port)
